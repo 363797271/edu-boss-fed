73f1a93957485dd61a8bacd531410fc8978a42de
张英丹 <15683901122@139.com>
2021-01-08 15:18:01 +0800

    Vercel 配置服务端代理

---
+++ server/proxy.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+module.exports = (req, res) => {
+  let target = ''
+
+  // 代理目标地址
+  if (req.url.startsWith('/front/')) {
+    target = 'http://edufront.lagou.com/'
+  } else if (req.url.startsWith('/boss/')) {
+    target = 'http://eduboss.lagou.com/'
+  }
+
+  // 创建代理对象并转发请求
+  createProxyMiddleware({
+    target,
+    changeOrigin: true
+  })
+}
