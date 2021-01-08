2e7349345338534ae3f11a008bc8068809332ef5
张英丹 <15683901122@139.com>
2021-01-08 15:41:42 +0800

    1

--- src/router/index.ts
+++ src/router/index.ts
@@ -127,11 +127,8 @@ const router = new VueRouter({
 // from: 从哪里来的路由信息
 // next: 通行的标志，路由守卫中一定要调用 next，否则页面无法展示
 router.beforeEach((to, from, next) => {
-  if (to.path.startsWith('/front') || to.path.startsWith('/boss')) {
-    // API 接口直接放行
-    next()
-  } else if (to.matched.some(record => record.meta.requiresAuth)) {
-    // to.matched 是一个数组（匹配到的路由记录）
+  // to.matched 是一个数组（匹配到的路由记录）
+  if (to.matched.some(record => record.meta.requiresAuth)) {
     if (!store.state.user) {
       next({
         name: 'login',
