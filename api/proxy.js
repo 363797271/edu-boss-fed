ffd4ca4715fcba7893051331918d81e23963f7ce
张英丹 <15683901122@139.com>
2021-01-08 16:06:40 +0800

    1

--- api/proxy.js
+++ api/proxy.js
@@ -5,10 +5,10 @@ module.exports = (req, res) => {
   let target = ''
 
   // 代理目标地址
-  if (req.url.startsWith('/front/')) {
-    target = 'http://edufront.lagou.com/'
-  } else if (req.url.startsWith('/boss/')) {
-    target = 'http://eduboss.lagou.com/'
+  if (req.url.startsWith('/front')) {
+    target = 'http://edufront.lagou.com'
+  } else if (req.url.startsWith('/boss')) {
+    target = 'http://eduboss.lagou.com'
   }
 
   // 创建代理对象并转发请求
